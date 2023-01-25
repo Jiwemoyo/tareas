44b687c17bbf8b1054990239ba7dd6c97ef7b8d6
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-01-25 15:34:43 -0500

    subiendo tarea

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def aniobisiesto(anio):
+    if anio%4==0 and anio%100!=0:
+        return("el año es bisiesto")
+    else:
+        return("el año no es bisiesto")
+    
+print(aniobisiesto(1992))       
