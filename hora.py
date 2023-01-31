7ac62a44481caca8836a6b69715c81782fb57800
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-01-31 16:00:32 -0500

    script

--- hora.py
+++ hora.py
@@ -2,7 +2,8 @@ import datetime
 
 hora_actual = datetime.datetime.now()
 
-if hora_actual.hour < 19:
-    print("Todavia se puede jugar")
+if hora_actual.hour > 19:
+    print("es hora de ir a casa")
 else:
-    print("es hora de volver a casa")
+    salida= 17 - hora_actual.hour 
+    print(f"faltan {salida} para irse a casa")
