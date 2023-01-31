927d42a19d963a603fd4d8a219b7cd7f4029ef17
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-01-31 15:56:11 -0500

    pquena correccion en la hora

--- hora.py
+++ hora.py
@@ -2,7 +2,7 @@ import datetime
 
 hora_actual = datetime.datetime.now()
 
-if hora_actual.hour < 17:
+if hora_actual.hour < 19:
     print("Todavia se puede jugar")
 else:
     print("es hora de volver a casa")
