94038ed691997b10cbca4e155aa0841df6def9b6
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-02-11 14:00:06 -0500

    serializacion

--- practica2input/ejercicio2.py
+++ practica2input/ejercicio2.py
@@ -1,7 +1,5 @@
-#guardar un objeto en un fichero para poder recuperarlos
-#serializar: convertir una representacion de un programa a una secuenciade datos que se pueden entender en un fichero
-#deserializar: tomar una secuancia de datos y converirlo en un programa legible (lo contrario de serializar)
-import pickle #permite serializar
+
+import pickle 
 
 class Vehiculo:
     color=""
@@ -18,20 +16,13 @@ class Vehiculo:
 
 
 
-# auto1=Vehiculo('rojo','nuevo',200000)
 
-# f=open('practica2input/datos.bin','wb')
-
-# pickle.dump(auto1,f) #prime parametro lo que quiero seializar , segunof parametro donde lo voy a serializar
-
-# f.close()
 
 f=open('practica2input/datos.bin','rb')
 
-carro2=pickle.load(f)
+carro2=pickle.load(f) 
 
 f.close()
-
 
 print(carro2.caracteristicas())
 
