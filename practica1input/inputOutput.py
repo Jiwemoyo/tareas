f7bb88d6ffdb3f5522f9362112ad0736f7011f10
Jiwemoyo <97122185+Jiwemoyo@users.noreply.github.com>
2023-02-11 13:32:34 -0500

    primer ejerciciio input y output

---
+++ practica1input/inputOutput.py
@@ -0,0 +1,34 @@
+f=open('practica1input/cuento.txt','w')
+
+cuento=f.write("Hola estoy creando un archivo txt de un cuento")
+
+
+
+
+
+f=open('practica1input/cuento.txt','r')
+cuento2 = f.read()
+print(cuento2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
